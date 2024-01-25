dd96488604ea63ee09e6c15e96ededbb4ca9f7a5
James Walker <walkah@walkah.net>
2024-01-25 17:46:26 -0500

    Add homestar (#1)
    * add homestar package, updates and cleanup
    
    * fix github action

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,7 +1,5 @@
 { pkgs, fission, system, ... }: {
-  carmirror = pkgs.callPackage ./carmirror/default.nix { };
-  kubo_carmirror = pkgs.callPackage ./carmirror/kubo.nix { };
-
+  homestar = pkgs.callPackage ./homestar.nix { };
   fission-cli = fission.packages.${system}.fission-cli;
   fission-server = fission.packages.${system}.fission-server;
 }
