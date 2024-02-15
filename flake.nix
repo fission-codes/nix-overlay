ec0f48099e9f745a6b5d611b074e2e884859cfa8
James Walker <walkah@walkah.net>
2024-02-15 12:57:45 -0500

    Simplify the overlay (#3)
    * simplify overlay - re-use each project's flake
    
    * update fission-server

--- flake.nix
+++ flake.nix
@@ -10,23 +10,29 @@
     };
 
     fission.url = "github:fission-codes/fission/walkah/haskell-nix";
+    homestar.url = "github:ipvm-wg/homestar/homestar-runtime-v0.1.1";
+    fission-server.url = "github:fission-codes/fission-server";
   };
 
-  outputs = { nixpkgs, flake-utils, fission, ... }:
+  outputs = { self, nixpkgs, flake-utils, fission, homestar, fission-server, ... }:
     flake-utils.lib.eachDefaultSystem
       (system:
         let pkgs = nixpkgs.legacyPackages.${system};
         in
         {
-          packages = import ./pkgs {
-            inherit fission pkgs system;
+          packages = {
+            homestar = homestar.packages.${system}.default;
+            fission-server = fission-server.packages.${system}.default;
+            fission-cli = fission.packages.${system}.fission-cli;
+            web-api = fission.packages.${system}.fission-server;
           };
-
           devShells.default = pkgs.mkShell {
             buildInputs = with pkgs; [ jq ];
           };
         }
       ) // {
-      overlay = final: _prev: import ./pkgs { inherit fission; system = final.system; pkgs = final; };
+      overlay = final: prev: {
+        inherit (self.packages.${prev.system}) homestar fission-server fission-cli web-api;
+      };
     };
 }
--- pkgs/default.nix
+++
@@ -1,7 +0,0 @@
-{ pkgs, fission, system, ... }: {
-  fission-server = pkgs.callPackage ./fission-server.nix { };
-  homestar = pkgs.callPackage ./homestar.nix { };
-
-  fission-cli = fission.packages.${system}.fission-cli;
-  web-api = fission.packages.${system}.fission-server;
-}
